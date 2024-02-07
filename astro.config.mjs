79ec1f072c3a992a1b77b750b0dbc5c9cf6f2858
Darwin Talay <1843525+dtalay@users.noreply.github.com>
2024-02-07 23:35:13 +0100

    feat: add mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dtalay.github.io',
-  integrations: [react()]
+  integrations: [react(), mdx()]
 });
