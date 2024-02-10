c411b7d8fe8adfbfbac01d4ae8fc8a66d41c4066
Darwin Talay <1843525+dtalay@users.noreply.github.com>
2024-02-10 21:57:47 +0100

    feat: implement front page layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                'cyan-400': "#87D9DE",
+                'gray-900': "#231F20",
+                'primary-black': "#231F20"
+            }
+        },
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
 }
